b1bb629833e81e78b94e895a6901cd0f422dc7d7
HarenU <hupadh4@gmail.com>
2022-11-21 11:56:52 +0000

    updated Tests

--- test_main.py
+++ test_main.py
@@ -3,8 +3,8 @@ from main import Order
 import json
 
 
-# def test_bucket():
-#     assert main.calculateBuckets(19.6) == [1,1,1,3]
+def test_bucket():
+    assert main.calculateBuckets(19.6) == [1,1,1,3]
 
 def test_write():
     order = Order("Dulux","White","13.00",[1,1,1,3])
